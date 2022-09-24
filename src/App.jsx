8d1b0af2602f9a1091d424306fe429975d298d0a
Sagar Kapoor <sagrkapr@gmail.com>
2022-09-25 00:03:42 +0300

    Header added to index.html

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import ListUser from './components/ListUser';
 import CreateUser from './components/CreateUser';
 import UpdateUser from './components/EditUser';
 import EditUser from './components/EditUser';
-import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <h5>React CRUD operations using PHP API and MySQL</h5>
       <BrowserRouter>
       <nav>
--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-const Header = () => {
-  return (
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-  )
-}
-
-export default Header
